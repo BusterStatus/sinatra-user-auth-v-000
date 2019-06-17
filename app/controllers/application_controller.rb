746da871cf183430e45782454c54d8f8aa6cd882
Matthew Serre <matthew.serre@gmail.com>
2019-06-17 22:06:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
@@ -24,6 +23,11 @@ class ApplicationController < Sinatra::Base
 
     redirect '/users/home'
   end
+  
+  get '/users/home' do
+    @user = User.find(session[:user_id])
+    erb :'/users/home'
+  end
 
   get '/sessions/login' do
 
@@ -32,9 +36,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
+    puts params
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:user_id] = @user.id
+    if session[:user_id] == @user.id
       redirect '/users/home'
     end
     redirect '/sessions/login'
@@ -43,11 +48,5 @@ class ApplicationController < Sinatra::Base
   get '/sessions/logout' do
     session.clear
     redirect '/'
-  end
-
-  get '/users/home' do
-
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
   end
 end
