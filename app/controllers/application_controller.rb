f10603dd6817c74bccc5a010810b9d006f0fdc8c
Matthew Serre <matthew.serre@gmail.com>
2019-06-17 22:32:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/users/home' do
-    @user = User.find_by(id :session[:user_id])
+    @user = User.find_by(id: session[:user_id])
     erb :'/users/home'
   end
 
